5ed7c1374e2353ef3c05f4597ea170f118b96208
themostretarded <marcosresidentevil@hotmail.com>
2022-02-11 18:12:22 -0600

    pichis cambios

--- Client/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
+++ Client/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
@@ -13,70 +13,70 @@ namespace BlazingChat.Client
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
+#line 1 "c:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
+#line 2 "c:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
+#line 3 "c:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
+#line 4 "c:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
+#line 5 "c:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
+#line 6 "c:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
+#line 7 "c:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
+#line 8 "c:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
+#line 9 "c:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
 using BlazingChat.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
+#line 10 "c:\Users\Meer\Desktop\pichitutorial\BlazingChat\Client\_Imports.razor"
 using BlazingChat.Client.Shared;
 
 #line default
